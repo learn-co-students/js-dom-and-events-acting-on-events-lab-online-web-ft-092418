4dd3d1dc454198e7f6a38c99877b702a1bd36a63
harryFBloch <harryfbloch@gmail.com>
2019-01-17 11:20:48 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+$(document).ready(function() {
+  retrieveEmployeeInformation()
+});
+
+const retrieveEmployeeInformation = function(){
+  return $('input[name=name]').val()
+}
+
+function addNewElementAsLi(){
+  $('.employee-list').append(`<li>${retrieveEmployeeInformation()}</li>`)
+}
+
+function addNewLiOnClick(){
+  addNewElementAsLi()
+  $('input[name=name]').val("")
+}
+
+function clearEmployeeListOnLinkClick(){
+  $('a').click(function (){
+    $('.employee-list').empty()
+  })
+}
