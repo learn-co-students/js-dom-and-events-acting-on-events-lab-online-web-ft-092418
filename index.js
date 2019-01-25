29663e2d5eff7dfaaf410c7c9949e4c5db4fa43f
Peter-G-Stone <peter.stone.online@gmail.com>
2019-01-25 14:36:42 -0500

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,66 @@ function preventRefreshOnSubmit(){
     })
 }
 
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  // my solution:
+  // let input = retrieveEmployeeInformation()
+  // let li = document.createElement('li')
+  // li.innerHTML = input
+  // const ul = document.querySelector('.employee-list')
+  // ul.appendChild(li)
+  // document.querySelector('input').value = ''
+  // return
+
+  // solution solution:
+
+  let employeeName = retrieveEmployeeInformation()
+
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick() {
+  const submit = document.querySelector('input[type="submit"]')
+
+  submit.addEventListener("click", function(event){
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  
+  //my solution ver1 and ver2
+  // const link = document.querySelector('a')
+  // const ul = document.getElementsByClassName('employee-list')[0]
+  // const lengthOfUl = ul.children.length
+  // // link.addEventListener("click", () => {
+  // //   for (let i = 0; i < lengthOfUl; i++) {
+  // //     ul.children[0].remove()
+  // //   }
+  // //   ul.innerHTML = ''
+  // //   return
+  // // })
+  
+  // link.addEventListener('click', () => ul.innerHTML = '')
+  // return
+
+
+  // solution solution
+
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
+
+function resetInput() {
+  document.querySelector('input').value = ''
+}
+
 preventRefreshOnSubmit()
+
