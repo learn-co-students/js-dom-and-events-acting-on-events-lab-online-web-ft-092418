d911d1e68797b80d979948d654e314abf3ac1ba1
endotnick <endotnick@gmail.com>
2019-02-11 13:34:16 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
+      event.preventDefault();
     })
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+let input = document.querySelector('input');
+
+const retrieveEmployeeInformation = () => {
+  return input.value;
+}
+
+const addNewElementAsLi = () => {
+  let employeeName = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`);
+}
+
+const addNewLiOnClick = () => {
+  const submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi();
+    input.value = '';
+  });
+}
+
+const clearEmployeeListOnLinkClick = () => {
+  let link = document.querySelector('a');
+  let list = document.querySelector('.employee-list');
+  link.addEventListener('click', function(event){
+    list.innerHTML = '';
+  })
+}
